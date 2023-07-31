fe445eae7fa341a4b19e59518bdd466ca8e79e2c
Dmitrii Iakovtsev <49165784+casual-impression@users.noreply.github.com>
2023-07-31 18:18:27 +0700

    Delete Observer/Strategy Pattern directory

--- Observer/Strategy Pattern/Assets/Scripts/ClassHelper/SpeakBehaviourCarrot.cs
+++
@@ -1,9 +0,0 @@
-using TMPro;
-
-public class SpeakBehaviourCarrot: SpeakBehaviourAbstract, ISpeak {
-    public SpeakBehaviourCarrot(TextMeshProUGUI text): base(text) {}
-    
-    public override void Speak() {
-        ShowMessage(_text, AnimalTypesEnum.Carrot);
-    }
-}
--- Observer/Strategy Pattern/Assets/Scripts/Enums/AnimalTypesEnum.cs
+++
@@ -1,7 +0,0 @@
-public enum AnimalTypesEnum {
-    Null = 0,
-    Carrot = 1,
-    Cat = 2,
-    Dog = 3,
-    Duck = 4
-}
